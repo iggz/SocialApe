7cfe52e605593646005f0a1c18ccb3a0d3386c0a
igor popenov <ipopenov@protonmail.com>
2019-11-10 12:31:39 -0500

    Established beginning API endpoints. About to add user authentication

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: '2019-11-09T02:59:07.278Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
