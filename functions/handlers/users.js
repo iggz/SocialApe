209ea42bd95f1a44fdcbe19755d5770662865c83
igor popenov <ipopenov@protonmail.com>
2019-11-15 23:23:58 -0500

    added details route (bio, website, location)

--- functions/handlers/users.js
+++ functions/handlers/users.js
@@ -5,7 +5,7 @@ const config = require('../util/config');
 const firebase = require('firebase');
 firebase.initializeApp(config)
 
-const { validateSignupData, validateLoginData } = require('../util/validators');
+const { validateSignupData, validateLoginData, reduceUserDetails } = require('../util/validators');
 
 
 // Sign up users
@@ -63,6 +63,7 @@ exports.signup = (req, res) => {
         });
 }
 
+// Log user in
 exports.login = (req, res) => {
     const user = {
         email: req.body.email,
@@ -71,9 +72,7 @@ exports.login = (req, res) => {
 
     const { valid, errors } = validateLoginData(user);
 
-
     if (Object.keys(errors).length > 0) return res.status(400).json(errors);
-
     firebase
         .auth()
         .signInWithEmailAndPassword(user.email, user.password)
@@ -92,6 +91,43 @@ exports.login = (req, res) => {
         });
 }
 
+// Add user details
+exports.addUserDetails = (req, res) => {
+    let userDetails = reduceUserDetails(req.body);
+
+    db.doc(`/users/${req.user.handle}`).update(userDetails)
+        .then(() => {
+            return res.json({ message: 'Details added successfully' })
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+}
+// Get own user details
+
+exports.getAuthenticatedUser = (req, res) => {
+    let userData = {};
+    db.doc(`/users/${req.user.handle}`).get()
+        .then(doc => {
+            if (doc.exists) {
+                userData.credentials = doc.data();
+                return db.collection('likes').where('userHandle', '==', req.user.handle).get()
+            }
+        })
+        .then(data => {
+            userData.likes = [];
+            data.forEach(doc => {
+                userData.likes.push(doc.data());
+            });
+            return res.json(userData);
+        })
+        .catch(err => {
+            console.error(err);
+            return res.status(500).json({ error: err.code });
+        })
+}
+// Upload a profile image for user
 exports.uploadImage = (req, res) => {
     const BusBoy = require('busboy');
     const path = require('path');
